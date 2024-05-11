562d32656995021dba57dd66dec59dbf351c463e
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-11 11:16:08 +0200

    Some projects are flat and buildable

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -3,3 +3,7 @@ global using System;
 global using System.Linq;
 global using System.Text;
 global using System.Data;
+global using System.Collections.Generic;
+global using System.Collections;
+global using System.IO;
+
