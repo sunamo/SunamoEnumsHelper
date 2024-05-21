bbab49c0018301b2d714fd40f2eef657c1d17c87
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-21 20:36:29 +0200

    Everyone on nuget

---
+++ _sunamo/SunamoValues/Constants/CodeElementsConstants.cs
@@ -0,0 +1,10 @@
+namespace SunamoEnumsHelper;
+
+
+public class CodeElementsConstants
+{
+    public const string NopeValue = "Nope";
+    public const string NoneValue = "None";
+    public const string SingleCommentCsharp = "//";
+    public const string XmlDocumentationCsharp = "///";
+}
