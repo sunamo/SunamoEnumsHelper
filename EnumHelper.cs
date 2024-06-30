27a12907a11c363ad226e8615cab2e6f8de7aa88
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-30 15:42:46 +0200

    All buildable, merged partial classes

--- EnumHelper.cs
+++ EnumHelper.cs
@@ -1,6 +1,6 @@
 namespace SunamoEnumsHelper;
 
-public static partial class EnumHelper
+public static class EnumHelper
 {
     public static string EnumToString<T>(T ds) where T : Enum
     {
@@ -250,5 +250,100 @@ public static partial class EnumHelper
         {
             max |= values[i];
         }
+    }
+
+    /// <summary>
+    /// GET WITHOUT NOPE (parse string, not numeric), USE METHOD WITH MORE ARGS
+    /// Can be use only for int enums
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    #region GetValues - unlike GetAllValues in EnumHelper.cs can exclude Nope,Shared, etc.
+    ///
+    public static List<T> GetValues<T>()
+        where T : struct
+    {
+        return GetValues<T>(false, true);
+    }
+    /// <summary>
+    /// Get all values expect of Nope/None
+    /// </summary>
+    /// <typeparam name = "T"></typeparam>
+    /// <param name = "type"></param>
+    public static List<T> GetValues<T>(bool IncludeNope, bool IncludeShared)
+        where T : struct
+    {
+        var type = typeof(T);
+        var values = Enum.GetValues(type).Cast<T>().ToList();
+        T nope;
+        if (!IncludeNope)
+        {
+            if (Enum.TryParse<T>(CodeElementsConstants.NopeValue, out nope))
+            {
+                values.Remove(nope);
+            }
+        }
+
+        if (!IncludeShared)
+        {
+            if (type.Name == "MySites")
+            {
+                if (Enum.TryParse<T>("Shared", out nope))
+                {
+                    values.Remove(nope);
+                }
+            }
+            else
+            {
+                if (Enum.TryParse<T>("Sha", out nope))
+                {
+                    values.Remove(nope);
+                }
+            }
+        }
+
+        if (Enum.TryParse<T>(CodeElementsConstants.NoneValue, out nope))
+        {
+            values.Remove(nope);
+        }
+
+        return values;
+    }
+    #endregion
+
+    public static List<string> GetFlags<T>(T key) where T : Enum
+    {
+        List<string> ls = new List<string>();
+        var v = Enum.GetValues(typeof(T));
+
+        foreach (Enum item in v)
+        {
+            if (key.HasFlag(item))
+            {
+                ls.Add(item.ToString());
+            }
+        }
+        return ls;
+    }
+
+    /// <summary>
+    /// ignore case. 
+    /// A1 must be, default(T) cant be returned because in comparing default(T) is always true for any value of T
+    /// </summary>
+    /// <typeparam name = "T"></typeparam>
+    /// <param name = "web"></param>
+    public static T Parse<T>(string web, T _def, bool returnDefIfNull = false)
+        where T : struct
+    {
+        if (returnDefIfNull)
+        {
+            return _def;
+        }
+        T result;
+        if (Enum.TryParse<T>(web, true, out result))
+        {
+            return result;
+        }
+
+        return _def;
     }
 }
